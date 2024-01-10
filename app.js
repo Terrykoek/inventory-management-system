8a41919c94143375fec9c4db18fa848ed4c334cd
TerryKoek <terrykoekkaijun@gmail.com>
2024-01-10 10:58:30 +0800

    create table on AWS

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const AWS = require('aws-sdk');
+const { DynamoDBClient, ListTablesCommand } = require('@aws-sdk/client-dynamodb');
 
-// Update these with your AWS credentials and DynamoDB region
-AWS.config.update({
-  accessKeyId: 'YOUR_ACCESS_KEY',
-  secretAccessKey: 'YOUR_SECRET_KEY',
-  region: 'YOUR_REGION',
+const client = new DynamoDBClient({
+  region: 'ap-southeast-1',
+  credentials: {
+    accessKeyId: 'AKIA47CRX2C4JZJE2YLT',
+    secretAccessKey: 'NRvzD7HMs8F47Wl5GzveB8bgsdnLfHFGfyBRKzv1',
+  },
 });
-
-const dynamodb = new AWS.DynamoDB();
 
 // Example: Listing tables
-dynamodb.listTables({}, (err, data) => {
-  if (err) {
-    console.error('Error listing tables:', err);
-  } else {
+const command = new ListTablesCommand({});
+client.send(command)
+  .then(data => {
     console.log('Tables:', data.TableNames);
-  }
-});
+  })
+  .catch(error => {
+    console.error('Error listing tables:', error);
+  });
